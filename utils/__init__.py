284ca675ee3d01eede6d3d800d5341c3ab43cabe
vrushanktadepalli <vrushanktadepalli9@gmail.com>
2026-02-09 22:10:49 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,9 @@
-from gifs_show import cycle_gifs, display_gif
-from images_show import cycle_images, display_image
-from patterns import dvd5px, matrix_rain, spiral
-from quotes import nextQuote, randomQuote
-from scroll import (
+from . import gifs_show, images_show, patterns, quotes, scroll, videos_show
+from .gifs_show import cycle_gifs, display_gif
+from .images_show import cycle_images, display_image
+from .patterns import dvd5px, matrix_rain, spiral
+from .quotes import nextQuote, randomQuote
+from .scroll import (
     blink_text,
     display_text,
     scroll_down,
@@ -12,9 +13,7 @@ from scroll import (
     typewriter_text,
     wave_text,
 )
-from videos_show import cycle_videos, display_video
-
-from . import gifs_show, images_show, patterns, quotes, scroll, videos_show
+from .videos_show import cycle_videos, display_video
 
 __all__ = [
     'videos_show',
